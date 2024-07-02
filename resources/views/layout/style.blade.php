1e4f2fc7d61df026cf7e5bc1ef5dd481df094d0b
citrafadilah <citrafadilahhasibuan_2226240089@mhs.mdp.ac.id>
2024-07-02 20:26:57 +0700

    fixing vercel

--- resources/views/layout/style.blade.php
+++ resources/views/layout/style.blade.php
@@ -5,26 +5,26 @@
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome Icons -->
-<link rel="stylesheet" href="{{ asset ('/AdminLTE/plugins/fontawesome-free/css/all.min.css')}}">
+<link rel="stylesheet" href="{{ url ('/AdminLTE/plugins/fontawesome-free/css/all.min.css')}}">
 <!-- overlayScrollbars -->
-<link rel="stylesheet" href="{{ asset('/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-{{-- <link rel="stylesheet" href="{{ asset('/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}"> --}}
+<link rel="stylesheet" href="{{ url('/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+{{-- <link rel="stylesheet" href="{{ url('/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}"> --}}
 <!-- Theme style -->
-<link rel="stylesheet" href="{{ asset ('/AdminLTE/dist/css/adminlte.min.css') }}">
+<link rel="stylesheet" href="{{ url ('/AdminLTE/dist/css/adminlte.min.css') }}">
 {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
 
 {{-- tambah --}}
  <!-- DataTables -->
- <link rel="stylesheet" href="{{ asset('AdminLTE') }}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
- <link rel="stylesheet" href="{{ asset('AdminLTE') }}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
- <link rel="stylesheet" href="{{ asset('AdminLTE') }}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+ <link rel="stylesheet" href="{{ url('AdminLTE') }}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+ <link rel="stylesheet" href="{{ url('AdminLTE') }}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+ <link rel="stylesheet" href="{{ url('AdminLTE') }}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 
  <!-- Tempusdominus Bootstrap 4 -->
-<link rel="stylesheet" href="{{ asset('AdminLTE') }}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+<link rel="stylesheet" href="{{ url('AdminLTE') }}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
 
 <!-- Select2 -->
-<link rel="stylesheet" href="{{ asset('AdminLTE') }}/plugins/select2/css/select2.min.css">
-<link rel="stylesheet" href="{{ asset('AdminLTE') }}/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<link rel="stylesheet" href="{{ url('AdminLTE') }}/plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="{{ url('AdminLTE') }}/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 
 <!-- Daterange picker -->
-<link rel="{{ asset('stylesheet" href="plugins/daterangepicker/daterangepicker.css') }}">
+<link rel="{{ url('stylesheet" href="plugins/daterangepicker/daterangepicker.css') }}">
